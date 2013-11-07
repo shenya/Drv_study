bbb2096383e118df94c8130887ed46ed47649aec
sgy <shengybest@gmail.com>
2013-11-07 19:27:36 +0800

    write and read something

--- hello.c
+++ hello.c
@@ -1,44 +1,112 @@
+/*function: character deivce driver for study
+ *author: sgy
+ *date: 2013-11-07
+*/
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <asm/uaccess.h>
 
+
+#define DEVICE_BUSY 1
+#define DEVICE_SUCCESS 0
+#define KERNEL_DATLEN 1024
+
+//module info
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Guangyao Shen");
 MODULE_DESCRIPTION("driver study");
-
+//device name
 char *dev_name = "chr_hello";
 dev_t dev_id;
-int major;
+int major;   //major device number
 int minor;
+static int device_open = 0; //open flag
+
+static char kernel_buf[KERNEL_DATLEN];
+
+static int data_byte = 0; //record bytes counter
 
 int f_open(struct inode *inode, struct file *file);
 ssize_t f_write(struct file *file, const char __user *buf, size_t length, loff_t * offset);
 int f_release(struct inode *inode, struct file *file);
+ssize_t f_read(struct file *file, char __user *buf, size_t length, loff_t * offset);
 
 struct file_operations Fops=
 {
 	.open = f_open,
+	.read = f_read,
 	.write = f_write,
 	.release = f_release,
 };
 
 int f_open(struct inode *inode, struct file *file)
 {
+	if(device_open)
+	{
+		printk(KERN_ALERT "device busy");
+		return -DEVICE_BUSY;
+	}
 
+	device_open++;
 
-	return 0;
+	return DEVICE_SUCCESS;
 }
 
 int f_release(struct inode *inode, struct file *file)
 {
-
+	device_open--;
 
 	return 0;
 }
-ssize_t f_write(struct file *file, const char __user *buf, size_t length, loff_t * offset)
+ssize_t f_read(struct file *file, char __user *buf, size_t length, loff_t * offset)
 {
-	switch(buf[0])
+	int ret = 0;
+	unsigned long p = *offset;
+
+	if(length > KERNEL_DATLEN - p)
+	{
+		length = KERNEL_DATLEN - p;	
+	}
+	if(length > data_byte)
+	{
+		length = data_byte;
+	}
+	printk(KERN_ALERT "buf is: %s K-p %ld\n", kernel_buf, KERNEL_DATLEN - p);
+	ret = copy_to_user(buf, kernel_buf, length);
+	if(ret == 0)
+	{
+		
+		printk(KERN_ALERT "copy_to_user success\n");
+		ret = length;
+	}
+	else
+	{
+		printk(KERN_ALERT "copy_to_user fail\n");
+	}
+	return ret;
+}
+ssize_t f_write(struct file *file, const char __user *buf, size_t length, loff_t * offset)
+{	
+	int ret = 0;
+
+	ret = copy_from_user(kernel_buf, buf, length);
+	if(ret < 0)
+	{
+		printk(KERN_ALERT "copy_from_user fail\n");
+		return 0;
+	}
+	else
+	{
+		*offset += length;
+		ret = length;
+		data_byte = length;
+	}
+	
+	printk(KERN_ALERT "buf is: %s length %ld\n", kernel_buf, length);
+	switch(kernel_buf[0])
 	{
 		case 'a':
 						printk(KERN_ALERT "this is a\n");
@@ -47,11 +115,14 @@ ssize_t f_write(struct file *file, const char __user *buf, size_t length, loff_t
 						printk(KERN_ALERT "this is a\n");
 						break;
 						
-
+		default:
+						printk(KERN_ALERT "not support\n");
+						break;
 
 	}
+	printk(KERN_ALERT "wite %dbytes\n", ret);
 
-	return 0;
+	return ret;
 
 }
 
